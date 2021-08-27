8474928db9d88c1b48e5e570596b97e5376543a0
Jarfsoft <andresjex@hotmail.com>
2021-08-26 18:56:35 -0600

    Remove default files, add new component

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="btn"> </button>
+</template>
+
+<script>
+  export default {
+    name: 'Button'
+  }
+</script>
+
+<style>
+  .btn {
+    border-radius: 50%;
+    width: 100px;
+    height: 100px;
+    background-color: darkred;
+  }
+</style>
